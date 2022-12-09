93c3f2fdaf7fc0c87229c447a058027e0c9ae2f1
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-09 23:13:52 +0300

    add (arguments and format)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const { Command } = require('commander');
+import { Command } from 'commander';
 const program = new Command();
 
 
@@ -7,5 +7,8 @@ program
   .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
