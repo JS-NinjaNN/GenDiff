a40c0a93d8c2369763ac138a0f9dafbc4640e65b
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-12 00:09:26 +0300

    add gendiff json test

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/extensions */
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import gendiff from '../src/index.js';
+import expected1 from '../__fixtures__/expected1';
+import exp from 'constants';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('gendiff json', () => {
+  expect(gendiff(getFixturePath(file1.json), getFixturePath(file2.json))).toEqual(expected1());
+});
