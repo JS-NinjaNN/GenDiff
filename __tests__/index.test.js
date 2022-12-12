e731630a4f9296377dc4241c3f851c10832ea06b
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-12 21:54:34 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,12 +3,19 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import gendiff from '../src/index.js';
 import expected1 from '../__fixtures__/expected1.js';
+import { jest } from '@jest/globals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+beforeEach(() => {
+  console.log = jest.fn();
+});
+
 test('gendiff json', () => {
-  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(expected1());
+  gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const actual = console.log.mock.calls;
+  const expected = expected1();
+  expect(actual).toEqual(expected);
 });
