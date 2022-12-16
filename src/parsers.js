b2ccbd7da95c3254a8d48f11b20068954ce6ff68
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-16 23:22:29 +0300

    create parsers module

---
+++ src/parsers.js
@@ -0,0 +1,21 @@
+import { readFileSync } from 'fs';
+import { cwd } from 'node:process';
+import path from 'path';
+import YAML from 'js-yaml';
+
+const parseJson = (filepath) => JSON.parse(readFileSync(path.resolve(cwd(), filepath), 'utf-8'));
+
+const parseYaml = (filepath) => YAML.load(readFileSync(path.resolve(cwd(), filepath), 'utf-8'));
+
+export default (filepath) => {
+  switch (path.extname(filepath)) {
+    case '.json':
+      return parseJson(filepath);
+    case '.yaml':
+      return parseYaml(filepath);
+    case '.yml':
+      return parseYaml(filepath);
+    default:
+      throw new Error('The file name must be entered with the extension!');
+  }
+};
