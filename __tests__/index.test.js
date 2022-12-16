728aa73f4bbe57a32e00ed9c8533cefcfd7824f4
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-16 23:21:41 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/extensions */
+import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import { jest } from '@jest/globals';
 import gendiff from '../src/index.js';
 import expected1 from '../__fixtures__/expected1.js';
 
@@ -9,13 +9,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-beforeEach(() => {
-  console.log = jest.fn();
+test('gendiff json should work', () => {
+  const actual = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const expected = expected1();
+  expect(actual).toEqual(expected);
 });
 
-test('gendiff json', () => {
-  gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const actual = console.log.mock.calls.join('\n');
-  const expected = expected1().join('\n');
+test('gendiff yaml should work', () => {
+  const actual = gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'));
+  const expected = expected1();
+  expect(actual).toEqual(expected);
+});
+
+test('gendiff yml should work', () => {
+  const actual = gendiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+  const expected = expected1();
   expect(actual).toEqual(expected);
 });
