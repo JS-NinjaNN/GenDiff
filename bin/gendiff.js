d3e92b7952f5f84b8882adc7f7b598ac27d117e3
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-16 23:22:00 +0300

    update cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,5 +12,8 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 program.parse();
