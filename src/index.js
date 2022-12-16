fac0c9f98272314c31a856831fe6ebf3c8ad9c1f
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-16 23:22:14 +0300

    update main fun

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
+/* eslint-disable import/extensions */
 /* eslint-disable no-console */
-import { readFileSync } from 'fs';
 import _ from 'lodash';
-import path from 'path';
-import { cwd } from 'node:process';
-
-const parseFile = (filepath) => JSON.parse(readFileSync(path.resolve(cwd(), filepath), 'utf-8'));
+import parseFile from './parsers.js';
 
 export default (filepath1, filepath2) => {
   const obj1 = parseFile(filepath1);
@@ -12,21 +9,16 @@ export default (filepath1, filepath2) => {
   const uniqKeys = _.uniq(Object.keys(obj1), Object.keys(obj2));
 
   const diff = uniqKeys.reduce((acc, key) => {
-    const bothHasKey = Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key);
-    let newString = '';
-    if (bothHasKey) {
-      // prettier-ignore
-      newString = obj1[key] === obj2[key]
-        ? `    ${key}: ${obj1[key]}`
-        : `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-    } else {
-      newString = Object.hasOwn(obj1, key)
-        ? `  - ${key}: ${obj1[key]}`
-        : `  + ${key}: ${obj2[key]}`;
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      if (obj1[key] === obj2[key]) {
+        return [...acc, `    ${key}: ${obj1[key]}`];
+      }
+      return [...acc, `  - ${key}: ${obj1[key]}`, `  + ${key}: ${obj2[key]}`];
     }
-    acc.push(newString);
-    return acc;
-  }, ['{']);
-  diff.push('}');
-  console.log(diff.join('\n'));
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      return [...acc, `  - ${key}: ${obj1[key]}`];
+    }
+    return [...acc, `  + ${key}: ${obj2[key]}`];
+  }, []);
+  return ['{', ...diff, '}'].join('\n');
 };
