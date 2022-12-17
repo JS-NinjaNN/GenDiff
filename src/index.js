5ea5bda44874662613242a6a445d3be7fa078abe
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-17 23:34:42 +0300

    update main fun

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ export default (filepath1, filepath2) => {
 
   const iter = (value1, value2) => {
     const keys = _.union(Object.keys(value1), Object.keys(value2));
-    const diff = keys.flatMap((currentKey) => {
-      console.log(currentKey);
+    const diff = keys.map((currentKey) => {
       let result;
       if (_.has(value1, currentKey) && _.has(value2, currentKey)) {
         if (!_.isObject(value1[currentKey]) && !_.isObject(value2[currentKey])) {
@@ -22,11 +21,15 @@ export default (filepath1, filepath2) => {
             };
           }
         } else if (_.isObject(value1[currentKey]) && _.isObject(value2[currentKey])) {
-          result = { key: currentKey, children: [iter(value1[currentKey], value2[currentKey])], type: 'nested' };
+          result = { key: currentKey, children: iter(value1[currentKey], value2[currentKey]), type: 'nested' };
         } else if (_.isObject(value1[currentKey]) && !_.isObject(value2[currentKey])) {
-
+          result = {
+            key: currentKey, value1: [value1[currentKey]], value2: value2[currentKey], type: 'changed',
+          };
         } else {
-
+          result = {
+            key: currentKey, value1: value1[currentKey], value2: [value2[currentKey]], type: 'changed',
+          };
         }
       } else if (_.has(value1, currentKey) && !_.has(value2, currentKey)) {
         if (!_.isObject(value1[currentKey])) {
