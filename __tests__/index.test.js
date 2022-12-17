c14c189af8880ba26d74c015915b6ac52e265b9d
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-17 22:53:05 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,28 +3,28 @@ import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import gendiff from '../src/index.js';
-import expectedPlain from '../__fixtures__/expectedPlain.js';
+import expectedDiff from '../__fixtures__/expected_diff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-describe('gendiff tests', () => {
-  test('gendiff plain json should work', () => {
-    const actual = gendiff(getFixturePath('plain1.json'), getFixturePath('plain2.json'));
-    const expected = expectedPlain();
+describe('gendiff generate correct diff', () => {
+  test('gendiff nested json should work', () => {
+    const actual = gendiff(getFixturePath('nested1.json'), getFixturePath('nested2.json'));
+    const expected = expectedDiff();
     expect(actual).toEqual(expected);
   });
 
   test('gendiff plain yaml should work', () => {
-    const actual = gendiff(getFixturePath('plain1.yaml'), getFixturePath('plain2.yaml'));
-    const expected = expectedPlain();
+    const actual = gendiff(getFixturePath('nested1.yaml'), getFixturePath('nested2.yaml'));
+    const expected = expectedDiff();
     expect(actual).toEqual(expected);
   });
 
   test('gendiff plain yml should work', () => {
-    const actual = gendiff(getFixturePath('plain1.yml'), getFixturePath('plain2.yml'));
-    const expected = expectedPlain();
+    const actual = gendiff(getFixturePath('nested1.yml'), getFixturePath('nested2.yml'));
+    const expected = expectedDiff();
     expect(actual).toEqual(expected);
   });
 });
