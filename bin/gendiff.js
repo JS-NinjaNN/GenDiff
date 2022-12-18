4a1e8cc73c4139171b4a061eb977854b0b1b89ac
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-18 22:25:07 +0300

    update cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,9 +11,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2);
+    const option = program.opts();
+    const diff = genDiff(filepath1, filepath2, option.format);
     console.log(diff);
   });
 program.parse();
