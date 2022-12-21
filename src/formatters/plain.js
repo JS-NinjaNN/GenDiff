8679ae20c495f8a73f51f5ca0c6257732098bf5b
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-21 23:32:57 +0300

    update main fun to switch cases

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,32 +10,39 @@ export default (diff) => {
       const newPath = (prevNodeType === 'nested')
         ? [...path, getKey(node)]
         : [];
-      if (getType(node) === 'added') {
-        return (_.isPlainObject(getValue(node)))
-          ? `Property '${newPath.join('.')}' was added with value: [complex value]`
-          : `Property '${newPath.join('.')}' was added with value: ${modifyValue(getValue(node))}`;
-      }
-      if (getType(node) === 'deleted') {
-        return `Property '${newPath.join('.')}' was removed`;
-      }
-      if (getType(node) === 'changed') {
-        const [value1, value2] = getValue(node);
-        if (_.isPlainObject(value1) || _.isPlainObject(value2)) {
-          return _.isPlainObject(value1)
-            ? `Property '${newPath.join('.')}' was updated. From [complex value] to ${modifyValue(value2)}`
-            : `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to [complex value]`;
+
+      switch (getType(node)) {
+        case 'added':
+          return (_.isPlainObject(getValue(node)))
+            ? `Property '${newPath.join('.')}' was added with value: [complex value]`
+            : `Property '${newPath.join('.')}' was added with value: ${modifyValue(getValue(node))}`;
+        case 'deleted':
+          return `Property '${newPath.join('.')}' was removed`;
+        case 'changed': {
+          const [value1, value2] = getValue(node);
+          if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+            return `Property '${newPath.join('.')}' was updated. From [complex value] to [compex value]`;
+          }
+          if (_.isPlainObject(value1) || _.isPlainObject(value2)) {
+            return _.isPlainObject(value1)
+              ? `Property '${newPath.join('.')}' was updated. From [complex value] to ${modifyValue(value2)}`
+              : `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to [complex value]`;
+          }
+          if (!_.isPlainObject(value1) && !_.isPlainObject(value2)) {
+            return `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to ${modifyValue(value2)}`;
+          }
+          break;
         }
-        if (!_.isPlainObject(value1) && !_.isPlainObject(value2)) {
-          return `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to ${modifyValue(value2)}`;
-        }
-      }
-      if (getType(node) === 'nested') {
-        const currentNodeType = getType(node);
-        return `${iter(getValue(node), newPath, currentNodeType)}`;
+        case 'nested':
+          return `${iter(getValue(node), newPath, getType(node))}`;
+        // no default
       }
       return '';
     });
-    return result
+    return [...result]
+      // Илья, вопрос. Почему тут пришлось использовать filter для избавления от пустых строк
+      // (в выводе выглядят как пропуск строки)
+      // хотя в stylish без него они не отображаются?
       .filter((item) => item !== '')
       .join('\n');
   };
