8c6145b49530b34899da24c1c2e7942cf6570e37
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-21 22:40:07 +0300

    fix returns

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,32 +8,32 @@ export default (diff) => {
   const iter = (tree, path, prevNodeType) => {
     const result = tree.map((node) => {
       const newPath = (prevNodeType === 'nested')
-        ? [...path]
+        ? [...path, getKey(node)]
         : [];
-      newPath.push(getKey(node));
-      let string = '';
       if (getType(node) === 'added') {
-        string = (_.isPlainObject(getValue(node)))
-          ? string = `Property '${newPath.join('.')}' was added with value: [complex value]`
+        return (_.isPlainObject(getValue(node)))
+          ? `Property '${newPath.join('.')}' was added with value: [complex value]`
           : `Property '${newPath.join('.')}' was added with value: ${modifyValue(getValue(node))}`;
-      } else if (getType(node) === 'deleted') {
-        string = `Property '${newPath.join('.')}' was removed`;
-      } else if (getType(node) === 'changed') {
+      }
+      if (getType(node) === 'deleted') {
+        return `Property '${newPath.join('.')}' was removed`;
+      }
+      if (getType(node) === 'changed') {
         const [value1, value2] = getValue(node);
         if (_.isPlainObject(value1) || _.isPlainObject(value2)) {
-          string = _.isPlainObject(value1)
+          return _.isPlainObject(value1)
             ? `Property '${newPath.join('.')}' was updated. From [complex value] to ${modifyValue(value2)}`
             : `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to [complex value]`;
-        } else {
-          string = `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to ${modifyValue(value2)}`;
         }
-      } else if (getType(node) === 'unchanged') {
-        return '';
-      } else if (getType(node) === 'nested') {
-        const currentNodeType = getType(node);
-        string = `${iter(getValue(node), newPath, currentNodeType)}`;
+        if (!_.isPlainObject(value1) && !_.isPlainObject(value2)) {
+          return `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to ${modifyValue(value2)}`;
+        }
       }
-      return string;
+      if (getType(node) === 'nested') {
+        const currentNodeType = getType(node);
+        return `${iter(getValue(node), newPath, currentNodeType)}`;
+      }
+      return '';
     });
     return result
       .filter((item) => item !== '')
