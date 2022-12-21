66e42f6e1cce8f14244601d63a0ee7b0a401f5bd
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-21 21:19:17 +0300

    delete let and test sort()

--- src/index.js
+++ src/index.js
@@ -9,35 +9,35 @@ export default (filepath1, filepath2, formatName = 'stylish') => {
   const data2 = parseFile(filepath2);
 
   const iter = (value1, value2) => {
-    const diff = _.union(Object.keys(value1), Object.keys(value2))
-      .sort()
-      .map((currentKey) => {
-        let result;
-        if (_.has(value1, currentKey) && _.has(value2, currentKey)) {
-          if (!_.isPlainObject(value1[currentKey]) && !_.isPlainObject(value2[currentKey])) {
-            result = (value1[currentKey] === value2[currentKey])
-              ? { key: currentKey, value: value1[currentKey], type: 'unchanged' }
-              : {
-                key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
-              };
-          } else if (_.isPlainObject(value1[currentKey]) && _.isPlainObject(value2[currentKey])) {
-            result = { key: currentKey, children: iter(value1[currentKey], value2[currentKey]), type: 'nested' };
-          } else if (_.isPlainObject(value1[currentKey]) && !_.isPlainObject(value2[currentKey])) {
-            result = {
+    const uniqKeys = _.union(Object.keys(value1), Object.keys(value2));
+    const sorted = [...uniqKeys].sort();
+    const diff = sorted.map((currentKey) => {
+      if (_.has(value1, currentKey) && _.has(value2, currentKey)) {
+        if (!_.isPlainObject(value1[currentKey]) && !_.isPlainObject(value2[currentKey])) {
+          return (value1[currentKey] === value2[currentKey])
+            ? { key: currentKey, value: value1[currentKey], type: 'unchanged' }
+            : {
               key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
             };
-          } else if (!_.isPlainObject(value1[currentKey]) && _.isPlainObject(value2[currentKey])) {
-            result = {
-              key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
-            };
-          }
-        } else {
-          result = (_.has(value1, currentKey) && !_.has(value2, currentKey))
-            ? { key: currentKey, value: value1[currentKey], type: 'deleted' }
-            : { key: currentKey, value: value2[currentKey], type: 'added' };
         }
-        return result;
-      });
+        if (_.isPlainObject(value1[currentKey]) && _.isPlainObject(value2[currentKey])) {
+          return { key: currentKey, children: iter(value1[currentKey], value2[currentKey]), type: 'nested' };
+        }
+        if (_.isPlainObject(value1[currentKey]) && !_.isPlainObject(value2[currentKey])) {
+          return {
+            key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
+          };
+        }
+        if (!_.isPlainObject(value1[currentKey]) && _.isPlainObject(value2[currentKey])) {
+          return {
+            key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
+          };
+        }
+      }
+      return (_.has(value1, currentKey) && !_.has(value2, currentKey))
+        ? { key: currentKey, value: value1[currentKey], type: 'deleted' }
+        : { key: currentKey, value: value2[currentKey], type: 'added' };
+    });
     return diff;
   };
   return chooseFormat(iter(data1, data2), formatName);
