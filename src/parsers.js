bde8adb84391b20f4ae52e4c48110fc0dd79e8d5
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-26 23:36:20 +0300

    update parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,21 +1,14 @@
-import { readFileSync } from 'fs';
-import { cwd } from 'process';
-import path from 'path';
 import YAML from 'js-yaml';
 
-const parseJson = (filepath) => JSON.parse(readFileSync(path.resolve(cwd(), filepath), 'utf-8'));
-
-const parseYaml = (filepath) => YAML.load(readFileSync(path.resolve(cwd(), filepath), 'utf-8'));
-
-export default (filepath) => {
-  switch (path.extname(filepath)) {
-    case '.json':
-      return parseJson(filepath);
-    case '.yaml':
-      return parseYaml(filepath);
-    case '.yml':
-      return parseYaml(filepath);
+export default (data, format) => {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+      return YAML.load(data);
+    case 'yml':
+      return YAML.load(data);
     default:
-      throw new Error('The file name must be entered with the extension!');
+      throw new Error(`Unknown format ${format}.`);
   }
 };
