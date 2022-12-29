82ab154827cddbecf2a25f41762e33d92612e812
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-30 01:52:52 +0300

    update getFormat, readFile

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
-/* eslint-disable import/extensions */
 import { readFileSync } from 'fs';
-import { cwd } from 'process';
 import path from 'path';
+import _ from 'lodash';
 import parseFile from './parsers.js';
 import format from './formatters/index.js';
 import buildTree from './treeBuilder.js';
 
-const readFile = (filepath) => {
-  const fullPath = path.resolve(cwd(), filepath);
-  const data = readFileSync(fullPath, 'utf-8');
-  return data;
-};
+const getFormat = (filepath) => _.trim(path.extname(filepath), '.');
 
-const getFormat = (filepath) => {
-  switch (path.extname(filepath)) {
-    case '.json':
-      return 'json';
-    case '.yaml':
-      return 'yaml';
-    case '.yml':
-      return 'yml';
-    default:
-      throw new Error('The file name must be entered with the extension!');
-  }
+const readFile = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath);
+  const data = readFileSync(fullPath, 'utf-8');
+  return parseFile(data, getFormat(filepath));
 };
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parseFile(readFile(filepath1), getFormat(filepath1));
-  const data2 = parseFile(readFile(filepath2), getFormat(filepath2));
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
   const diff = buildTree(data1, data2);
   return format(diff, formatName);
 };
