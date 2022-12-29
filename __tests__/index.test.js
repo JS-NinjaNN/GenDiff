4bffa50466d476cac06958966dd299dd5167e3b9
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-30 01:52:03 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/extensions */
 import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
@@ -8,23 +7,17 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixture = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectedStylish = readFileSync(getFixturePath('expectedStylish.txt'), 'utf-8');
-const expectedPlain = readFileSync(getFixturePath('expectedPlain.txt'), 'utf-8');
-const expectedJSON = readFileSync(getFixturePath('expectedJSON.txt'), 'utf-8');
-const formats = [
-  ['json'],
-  ['yaml'],
-  ['yml'],
-];
+const formats = ['json', 'yaml', 'yml'];
 
 describe('genDiff should work correctly', () => {
   test.each(formats)('genDiff should work with %p', (format) => {
     const filepath1 = getFixturePath(`nested1.${format}`);
     const filepath2 = getFixturePath(`nested2.${format}`);
-    expect(genDiff(filepath1, filepath2)).toEqual(expectedStylish);
-    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
-    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
-    expect(genDiff(filepath1, filepath2, 'json')).toEqual(expectedJSON);
+    expect(genDiff(filepath1, filepath2)).toEqual(readFixture('expectedStylish.txt'));
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(readFixture('expectedStylish.txt'));
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(readFixture('expectedPlain.txt'));
+    expect(genDiff(filepath1, filepath2, 'json')).toEqual(readFixture('expectedJSON.txt'));
   });
 });
