7c98544a2f3c18c17e162c1b11167da81f05baa9
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-30 21:34:07 +0300

    update iter() (add new const for keys)

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -1,24 +1,23 @@
 import _ from 'lodash';
 
 const iter = (value1, value2) => {
-  const keys = _.sortBy(_.union(Object.keys(value1), Object.keys(value2)))
-    .map((key) => {
-      if (_.has(value1, key) && !_.has(value2, key)) {
-        return { key, value: value1[key], type: 'deleted' };
-      }
-      if (!_.has(value1, key) && _.has(value2, key)) {
-        return { key, value: value2[key], type: 'added' };
-      }
-      if (_.isPlainObject(value1[key]) && _.isPlainObject(value2[key])) {
-        return { key, children: iter(value1[key], value2[key]), type: 'nested' };
-      }
-      return (value1[key] === value2[key])
-        ? { key, value: value1[key], type: 'unchanged' }
-        : {
-          key, value1: value1[key], value2: value2[key], type: 'changed',
-        };
-    });
-  return keys;
+  const keys = _.sortBy(_.union(Object.keys(value1), Object.keys(value2)));
+  return keys.map((key) => {
+    if (_.has(value1, key) && !_.has(value2, key)) {
+      return { key, value: value1[key], type: 'deleted' };
+    }
+    if (!_.has(value1, key) && _.has(value2, key)) {
+      return { key, value: value2[key], type: 'added' };
+    }
+    if (_.isPlainObject(value1[key]) && _.isPlainObject(value2[key])) {
+      return { key, children: iter(value1[key], value2[key]), type: 'nested' };
+    }
+    return (value1[key] === value2[key])
+      ? { key, value: value1[key], type: 'unchanged' }
+      : {
+        key, value1: value1[key], value2: value2[key], type: 'changed',
+      };
+  });
 };
 
 export default (data1, data2) => iter(data1, data2);
