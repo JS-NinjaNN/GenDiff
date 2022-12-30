1f68e6460112ea16654e50c8d69c5f6ca0c542ec
ToxicNN <moiseenko.artem94@yandex.ru>
2022-12-30 19:29:02 +0300

    update treeBuilder

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -1,35 +1,24 @@
 import _ from 'lodash';
 
 const iter = (value1, value2) => {
-  const diff = _.sortBy(_.union(Object.keys(value1), Object.keys(value2)))
-    .map((currentKey) => {
-      if (_.has(value1, currentKey) && _.has(value2, currentKey)) {
-        if (!_.isPlainObject(value1[currentKey]) && !_.isPlainObject(value2[currentKey])) {
-          return (value1[currentKey] === value2[currentKey])
-            ? { key: currentKey, value: value1[currentKey], type: 'unchanged' }
-            : {
-              key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
-            };
-        }
-        if (_.isPlainObject(value1[currentKey]) && _.isPlainObject(value2[currentKey])) {
-          return { key: currentKey, children: iter(value1[currentKey], value2[currentKey]), type: 'nested' };
-        }
-        if (_.isPlainObject(value1[currentKey]) && !_.isPlainObject(value2[currentKey])) {
-          return {
-            key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
-          };
-        }
-        if (!_.isPlainObject(value1[currentKey]) && _.isPlainObject(value2[currentKey])) {
-          return {
-            key: currentKey, value1: value1[currentKey], value2: value2[currentKey], type: 'changed',
-          };
-        }
+  const keys = _.sortBy(_.union(Object.keys(value1), Object.keys(value2)))
+    .map((key) => {
+      if (_.has(value1, key) && !_.has(value2, key)) {
+        return { key, value: value1[key], type: 'deleted' };
       }
-      return (_.has(value1, currentKey) && !_.has(value2, currentKey))
-        ? { key: currentKey, value: value1[currentKey], type: 'deleted' }
-        : { key: currentKey, value: value2[currentKey], type: 'added' };
+      if (!_.has(value1, key) && _.has(value2, key)) {
+        return { key, value: value2[key], type: 'added' };
+      }
+      if (_.isPlainObject(value1[key]) && _.isPlainObject(value2[key])) {
+        return { key, children: iter(value1[key], value2[key]), type: 'nested' };
+      }
+      return (value1[key] === value2[key])
+        ? { key, value: value1[key], type: 'unchanged' }
+        : {
+          key, value1: value1[key], value2: value2[key], type: 'changed',
+        };
     });
-  return diff;
+  return keys;
 };
 
 export default (data1, data2) => iter(data1, data2);
