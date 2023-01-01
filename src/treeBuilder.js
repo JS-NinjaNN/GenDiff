dcb90e9950c0ad1a1b5d41fd8d842545e6b27e3f
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-01 17:59:46 +0300

    rename iter to buildTree, rename arguments

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -1,23 +1,24 @@
 import _ from 'lodash';
 
-const iter = (value1, value2) => {
-  const keys = _.sortBy(_.union(Object.keys(value1), Object.keys(value2)));
+const buildTree = (data1, data2) => {
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   return keys.map((key) => {
-    if (_.has(value1, key) && !_.has(value2, key)) {
-      return { key, value: value1[key], type: 'deleted' };
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return { key, value: data1[key], type: 'deleted' };
     }
-    if (!_.has(value1, key) && _.has(value2, key)) {
-      return { key, value: value2[key], type: 'added' };
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      return { key, value: data2[key], type: 'added' };
     }
-    if (_.isPlainObject(value1[key]) && _.isPlainObject(value2[key])) {
-      return { key, children: iter(value1[key], value2[key]), type: 'nested' };
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, children: buildTree(data1[key], data2[key]), type: 'nested' };
     }
-    return (value1[key] === value2[key])
-      ? { key, value: value1[key], type: 'unchanged' }
-      : {
-        key, value1: value1[key], value2: value2[key], type: 'changed',
+    if (data1[key] !== data2[key]) {
+      return {
+        key, value1: data1[key], value2: data2[key], type: 'changed',
       };
+    }
+    return { key, value: data1[key], type: 'unchanged' };
   });
 };
 
-export default (data1, data2) => iter(data1, data2);
+export default buildTree;
