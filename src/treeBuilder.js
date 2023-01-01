1128728a6db8dd7263d46856d34e04fe63c97407
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-01 22:14:03 +0300

    update treeBuilder, shorthanded some if's

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -3,16 +3,16 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   return keys.map((key) => {
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      return { key, value: data1[key], type: 'deleted' };
-    }
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      return { key, value: data2[key], type: 'added' };
-    }
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return { key, children: buildTree(data1[key], data2[key]), type: 'nested' };
     }
-    if (data1[key] !== data2[key]) {
+    if (!_.has(data1, key)) {
+      return { key, value: data2[key], type: 'added' };
+    }
+    if (!_.has(data2, key)) {
+      return { key, value: data1[key], type: 'deleted' };
+    }
+    if (!_.isEqual(data1[key], data2[key])) {
       return {
         key, value1: data1[key], value2: data2[key], type: 'changed',
       };
