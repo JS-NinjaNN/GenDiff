e7166808b8077771b2de23c9233b8c184809bf61
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-02 05:12:27 +0300

    update modifyValue() and formatPlain()

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,49 +1,34 @@
 import _ from 'lodash';
 import { getType, getValue, getKey } from './utils.js';
 
-const modifyValue = (value) => ((typeof value === 'string') ? `'${value}'` : `${value}`);
+const modifyValue = (value) => {
+  if (_.isPlainObject(value)) {
+    return '[complex value]';
+  }
+  return ((typeof value === 'string') ? `'${value}'` : `${value}`);
+};
 
-export default (diff) => {
-  const iter = (tree, path, prevNodeType) => {
-    const result = tree.map((node) => {
+const formatPlain = (tree, path = [], prevNodeType = 'nested') => {
+  const lines = tree
+    .filter((node) => getType(node) !== 'unchanged')
+    .map((node) => {
       const newPath = (prevNodeType === 'nested')
         ? [...path, getKey(node)]
         : [];
-
       switch (getType(node)) {
         case 'added':
-          return (_.isPlainObject(getValue(node)))
-            ? `Property '${newPath.join('.')}' was added with value: [complex value]`
-            : `Property '${newPath.join('.')}' was added with value: ${modifyValue(getValue(node))}`;
+          return `Property '${newPath.join('.')}' was added with value: ${modifyValue(getValue(node))}`;
         case 'deleted':
           return `Property '${newPath.join('.')}' was removed`;
         case 'changed': {
           const [value1, value2] = getValue(node);
-          if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-            return `Property '${newPath.join('.')}' was updated. From [complex value] to [complex value]`;
-          }
-          if (_.isPlainObject(value1) || _.isPlainObject(value2)) {
-            return _.isPlainObject(value1)
-              ? `Property '${newPath.join('.')}' was updated. From [complex value] to ${modifyValue(value2)}`
-              : `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to [complex value]`;
-          }
-          if (!_.isPlainObject(value1) && !_.isPlainObject(value2)) {
-            return `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to ${modifyValue(value2)}`;
-          }
-          break;
+          return `Property '${newPath.join('.')}' was updated. From ${modifyValue(value1)} to ${modifyValue(value2)}`;
         }
-        case 'nested':
-          return `${iter(getValue(node), newPath, getType(node))}`;
         // no default
       }
-      return '';
+      return `${formatPlain(getValue(node), newPath, getType(node))}`;
     });
-    return [...result]
-      // Илья, вопрос. Почему тут пришлось использовать filter для избавления от пустых строк
-      // (в выводе выглядят как пропуск строки)
-      // хотя в stylish без него они не отображаются?
-      .filter((item) => item !== '')
-      .join('\n');
-  };
-  return iter(diff, [], 'nested');
+  return lines.join('\n');
 };
+
+export default formatPlain;
