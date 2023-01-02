0faa421b92fa8f2d64e17d5dc191da16f78ba64e
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-02 05:13:12 +0300

    add makeIndent(), update stringify and formatStylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,59 +1,50 @@
 import _ from 'lodash';
 import { getType, getValue, getKey } from './utils.js';
 
-const stringify = (value, replacer, spacesCount, currentDepth) => {
-  const iter = (currentValue, depth) => {
-    if (!_.isPlainObject(currentValue)) {
-      return `${currentValue}`;
-    }
-
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-  return iter(value, currentDepth);
+const makeIndent = (depth, type = '') => {
+  const spacesCount = 4;
+  const indentSize = depth * spacesCount;
+  const indent = ' ';
+  switch (type) {
+    case 'added':
+      return '+ '.padStart(indentSize, indent);
+    case 'deleted':
+      return '- '.padStart(indentSize, indent);
+    case 'bracket':
+      return indent.repeat(indentSize - spacesCount);
+    // no default
+  }
+  return indent.repeat(indentSize);
 };
 
-export default (diff) => {
-  const iter = (tree, depth) => {
-    const spacesCount = 4;
-    const indentSize = depth * spacesCount;
-    const indent = ' ';
-    const currentIndent = indent.repeat(indentSize);
-    const currentIndentForAdded = '+ '.padStart(indentSize, ' ');
-    const currentIndentForDeleted = '- '.padStart(indentSize, ' ');
-    const bracketIndent = indent.repeat(indentSize - spacesCount);
-    const result = tree.map((node) => {
-      switch (getType(node)) {
-        case 'added':
-          return `${currentIndentForAdded}${getKey(node)}: ${stringify(getValue(node), ' ', 4, depth + 1)}`;
-        case 'deleted':
-          return `${currentIndentForDeleted}${getKey(node)}: ${stringify(getValue(node), ' ', 4, depth + 1)}`;
-        case 'unchanged':
-          return `${currentIndent}${getKey(node)}: ${stringify(getValue(node), ' ', 4, depth + 1)}`;
-        case 'changed': {
-          const [value1, value2] = getValue(node);
-          return `${currentIndentForDeleted}${getKey(node)}: ${stringify(value1, ' ', 4, depth + 1)}\n${currentIndentForAdded}${getKey(node)}: ${stringify(value2, ' ', 4, depth + 1)}`;
-        }
-        case 'nested':
-          return `${currentIndent}${getKey(node)}: ${iter(getValue(node), depth + 1)}`;
-        // no default
+const stringify = (data, depth) => {
+  if (!_.isPlainObject(data)) {
+    return String(data);
+  }
+  const lines = Object
+    .entries(data)
+    .map(([key, value]) => `${makeIndent(depth)}${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${makeIndent(depth, 'bracket')}}`;
+};
+
+const formatStylish = (tree, depth = 1) => {
+  const lines = tree.map((node) => {
+    switch (getType(node)) {
+      case 'added':
+        return `${makeIndent(depth, 'added')}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
+      case 'deleted':
+        return `${makeIndent(depth, 'deleted')}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
+      case 'unchanged':
+        return `${makeIndent(depth)}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
+      case 'changed': {
+        const [value1, value2] = getValue(node);
+        return `${makeIndent(depth, 'deleted')}${getKey(node)}: ${stringify(value1, depth + 1)}\n${makeIndent(depth, 'added')}${getKey(node)}: ${stringify(value2, depth + 1)}`;
       }
-      return '';
-    });
-    return [
-      '{',
-      ...result,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-  return iter(diff, 1);
+      // no default
+    }
+    return `${makeIndent(depth)}${getKey(node)}: ${formatStylish(getValue(node), depth + 1)}`;
+  });
+  return `{\n${lines.join('\n')}\n${makeIndent(depth, 'bracket')}}`;
 };
+
+export default formatStylish;
