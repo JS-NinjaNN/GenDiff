b856cb0b097a89ccf3aa1062253bff1593cba13e
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-02 22:49:09 +0300

    update indent()

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,50 +1,46 @@
 import _ from 'lodash';
 import { getType, getValue, getKey } from './utils.js';
 
-const makeIndent = (depth, type = '') => {
+const indent = (depth, isFull = false) => {
   const spacesCount = 4;
   const indentSize = depth * spacesCount;
-  const indent = ' ';
-  switch (type) {
-    case 'added':
-      return '+ '.padStart(indentSize, indent);
-    case 'deleted':
-      return '- '.padStart(indentSize, indent);
-    case 'bracket':
-      return indent.repeat(indentSize - spacesCount);
-    // no default
+  const replacer = ' ';
+  if (isFull === true) {
+    return replacer.repeat(indentSize);
   }
-  return indent.repeat(indentSize);
+  return replacer.repeat(indentSize - 2);
 };
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return String(data);
   }
+  const isFull = true;
   const lines = Object
     .entries(data)
-    .map(([key, value]) => `${makeIndent(depth)}${key}: ${stringify(value, depth + 1)}`);
-  return `{\n${lines.join('\n')}\n${makeIndent(depth, 'bracket')}}`;
+    .map(([key, value]) => `${indent(depth, isFull)}${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${indent(depth).substring(2)}}`;
 };
 
 const formatStylish = (tree, depth = 1) => {
+  const isFull = true;
   const lines = tree.map((node) => {
     switch (getType(node)) {
       case 'added':
-        return `${makeIndent(depth, 'added')}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
+        return `${indent(depth)}+ ${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
       case 'deleted':
-        return `${makeIndent(depth, 'deleted')}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
+        return `${indent(depth)}- ${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
       case 'unchanged':
-        return `${makeIndent(depth)}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
+        return `${indent(depth, isFull)}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
       case 'changed': {
         const [value1, value2] = getValue(node);
-        return `${makeIndent(depth, 'deleted')}${getKey(node)}: ${stringify(value1, depth + 1)}\n${makeIndent(depth, 'added')}${getKey(node)}: ${stringify(value2, depth + 1)}`;
+        return `${indent(depth)}- ${getKey(node)}: ${stringify(value1, depth + 1)}\n${indent(depth)}+ ${getKey(node)}: ${stringify(value2, depth + 1)}`;
       }
       // no default
     }
-    return `${makeIndent(depth)}${getKey(node)}: ${formatStylish(getValue(node), depth + 1)}`;
+    return `${indent(depth, isFull)}${getKey(node)}: ${formatStylish(getValue(node), depth + 1)}`;
   });
-  return `{\n${lines.join('\n')}\n${makeIndent(depth, 'bracket')}}`;
+  return `{\n${lines.join('\n')}\n${indent(depth).substring(2)}}`;
 };
 
 export default formatStylish;
