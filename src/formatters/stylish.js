65a4acfcbacbebebfe9c8e95adc05386b5a72d24
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-04 01:15:04 +0300

    update stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import { getType, getValue, getKey } from './utils.js';
+
+const IS_FULL = true;
 
 const indent = (depth, isFull = false) => {
   const spacesCount = 4;
@@ -15,32 +16,33 @@ const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return String(data);
   }
-  const isFull = true;
   const lines = Object
     .entries(data)
-    .map(([key, value]) => `${indent(depth, isFull)}${key}: ${stringify(value, depth + 1)}`);
-  return `{\n${lines.join('\n')}\n${indent(depth).substring(2)}}`;
+    .map(([key, value]) => `${indent(depth + 1, IS_FULL)}${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${indent(depth, IS_FULL)}}`;
 };
 
-const formatStylish = (tree, depth = 1) => {
-  const isFull = true;
-  const lines = tree.map((node) => {
-    switch (getType(node)) {
-      case 'added':
-        return `${indent(depth)}+ ${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
-      case 'deleted':
-        return `${indent(depth)}- ${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
-      case 'unchanged':
-        return `${indent(depth, isFull)}${getKey(node)}: ${stringify(getValue(node), depth + 1)}`;
-      case 'changed': {
-        const [value1, value2] = getValue(node);
-        return `${indent(depth)}- ${getKey(node)}: ${stringify(value1, depth + 1)}\n${indent(depth)}+ ${getKey(node)}: ${stringify(value2, depth + 1)}`;
-      }
-      // no default
+const iter = (tree, depth = 1) => tree.map((node) => {
+  switch (node.type) {
+    case 'added':
+      return `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
+    case 'deleted':
+      return `${indent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
+    case 'unchanged':
+      return `${indent(depth, IS_FULL)}${node.key}: ${stringify(node.value, depth)}`;
+    case 'changed': {
+      return `${indent(depth)}- ${node.key}: ${stringify(node.value1, depth)}\n${indent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
     }
-    return `${indent(depth, isFull)}${getKey(node)}: ${formatStylish(getValue(node), depth + 1)}`;
-  });
-  return `{\n${lines.join('\n')}\n${indent(depth).substring(2)}}`;
-};
+    case 'nested': {
+      const lines = iter(node.children, depth + 1);
+      return `${indent(depth, IS_FULL)}${node.key}: {\n${lines.join('\n')}\n${indent(depth, IS_FULL)}}`;
+    }
+    default:
+      throw new Error(`Unknown type ${node.type}.\nSupported types: added, deleted, unchanged, changed and nested.`);
+  }
+});
 
-export default formatStylish;
+export default (tree) => {
+  const result = iter(tree, 1);
+  return `{\n${result.join('\n')}\n}`;
+};
