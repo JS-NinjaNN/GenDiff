ced4c8fd1bd8338aebc78ab1880f7b03a82f53d7
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-07 04:53:09 +0300

    update main fun

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,34 +1,34 @@
 import _ from 'lodash';
 
-const modifyValue = (value) => {
-  if (_.isPlainObject(value)) {
+const stringify = (value) => {
+  if (_.isObjectLike(value)) {
     return '[complex value]';
   }
-  return ((typeof value === 'string') ? `'${value}'` : `${value}`);
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return String(value);
 };
 
-const iter = (tree, path = [], prevNodeType = 'nested') => {
-  const lines = tree
-    .filter((node) => node.type !== 'unchanged')
-    .map((node) => {
-      const newPath = (prevNodeType === 'nested')
-        ? [...path, node.key]
-        : [];
-      switch (node.type) {
-        case 'added':
-          return `Property '${newPath.join('.')}' was added with value: ${modifyValue(node.value)}`;
-        case 'deleted':
-          return `Property '${newPath.join('.')}' was removed`;
-        case 'changed': {
-          return `Property '${newPath.join('.')}' was updated. From ${modifyValue(node.value1)} to ${modifyValue(node.value2)}`;
-        }
-        case 'nested':
-          return `${iter(node.children, newPath, node.type)}`;
-        default:
-          throw new Error(`Unknown type ${node.type}.\nSupported types: added, deleted, unchanged, changed and nested.`);
+const iter = (tree, path = [], prevNodeType = 'nested') => tree
+  .filter((node) => node.type !== 'unchanged')
+  .map((node) => {
+    const newPath = (prevNodeType === 'nested')
+      ? [...path, node.key]
+      : [];
+    switch (node.type) {
+      case 'added':
+        return `Property '${newPath.join('.')}' was added with value: ${stringify(node.value)}`;
+      case 'deleted':
+        return `Property '${newPath.join('.')}' was removed`;
+      case 'changed': {
+        return `Property '${newPath.join('.')}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
       }
-    });
-  return lines.join('\n');
-};
+      case 'nested':
+        return iter(node.children, newPath, node.type).join('\n');
+      default:
+        throw new Error(`Unknown type ${node.type}.\nSupported types: added, deleted, unchanged, changed and nested.`);
+    }
+  });
 
-export default iter;
+export default (tree) => iter(tree).join('\n');
