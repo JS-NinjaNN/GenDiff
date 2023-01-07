0ca962d9b959644028ac877e8a0e2de90e7ae4a1
ToxicNN <moiseenko.artem94@yandex.ru>
2023-01-07 04:52:39 +0300

    update buildTree()

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -3,14 +3,14 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
   return keys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, children: buildTree(data1[key], data2[key]), type: 'nested' };
-    }
     if (!_.has(data1, key)) {
       return { key, value: data2[key], type: 'added' };
     }
     if (!_.has(data2, key)) {
       return { key, value: data1[key], type: 'deleted' };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, children: buildTree(data1[key], data2[key]), type: 'nested' };
     }
     if (!_.isEqual(data1[key], data2[key])) {
       return {
